0b29388cdf3e9b802bda4103b394ea972d549c62
break <1535388406@qq.com>
2019-12-15 16:51:50 +0800

    第二天课程内容提交

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 导入接收路由
+// 导入接收 路由
 import router from './router/router.js'
 
-// 导入饿了么ui
+// 导入饿了么 ui
 import ElementUI from 'element-ui';
-// 导入饿了么的样式
+// 导入饿了么 的样式
 import 'element-ui/lib/theme-chalk/index.css';
 // 注册一下
 Vue.use(ElementUI);
@@ -16,7 +16,11 @@ import "./style/base.css";
 
 Vue.config.productionTip = false
 
+// 测试环境变量
+window.console.log(process.env.VUE_APP_BASEURL)
+
 new Vue({
   render: h => h(App),
-  router
+  // 挂载到Vue示例上 router
+  router// router:router
 }).$mount('#app')
